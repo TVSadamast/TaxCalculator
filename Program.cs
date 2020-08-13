130e0d9c0da034ac938e3ee9a887870ec0719144
Tanmay Sadamast <tanmay@promactInfo.com>
2020-08-13 18:18:16 +0530

    First Commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TaxCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            double salary;
+            string repeat;
+
+            do
+            {
+                Console.WriteLine("\nTax Calculator");
+
+                try
+                {
+                    Console.Write("Please enter your Age : ");
+                    age = int.Parse(Console.ReadLine());
+
+                    Console.Write("Please enter your Salary : ");
+                    salary = int.Parse(Console.ReadLine());
+
+                    TaxCalculator taxCalculator = new TaxCalculator(age, salary);
+                    taxCalculator.CalculateTax();
+                    Console.WriteLine("Total Payable Tax : " + taxCalculator._tax);
+
+                    Console.WriteLine("\nDo you want to Calculate Tax again?(Y/N)");
+                    repeat = Console.ReadLine();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("**Please enter valid data.");
+                    repeat = "Y";
+                }              
+
+            } while (repeat == "Y" || repeat == "y");
+        }
+    }
+}
